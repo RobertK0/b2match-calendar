a79beaddf0bc2eee969b3f99d909ed487267f23d
Zero0k <robykaurin@gmail.com>
2022-05-25 14:29:21 +0200

    Fix svg property naming

--- components/HomeHeader.tsx
+++ components/HomeHeader.tsx
@@ -68,11 +68,11 @@ const HomeHeader = () => {
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
-            stroke-width="2"
+            strokeWidth="2"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M15 19l-7-7 7-7"
             />
           </svg>
@@ -85,11 +85,11 @@ const HomeHeader = () => {
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
-            stroke-width="2"
+            strokeWidth="2"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M9 5l7 7-7 7"
             />
           </svg>
